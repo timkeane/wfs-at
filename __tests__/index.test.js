3d8464c718cc1cb00417003f37bc40b9b77563f2
timkeane <tkeane@doitt.nyc.gov>
2019-08-02 16:28:03 -0400

    .

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,18 +8,59 @@ const COORD_AT_2_MTC = [-8236095, 4967234]
 
 const wfsAt = require('../index')
 
-const options = {
-  coordinate: COORD_AT_2_MTC,
-  wfsEndpoint: WFS_ENDPOINT,
-  layer: LAYER,
-  geometryColumn: GEOM_COL,
-  dataProjection: DATA_PROJ
-}
+beforeEach(() => {
+  jest.setTimeout(60 * 1000)
+})
 
 test('wfsAt success', done => {
   expect.assertions(1)
+
+  const options = {
+    coordinate: COORD_AT_2_MTC,
+    wfsEndpoint: WFS_ENDPOINT,
+    layer: LAYER,
+    geometryColumn: GEOM_COL,
+    dataProjection: DATA_PROJ
+  }
+  
   wfsAt(options).then(features => {
     expect(features.features.length).toBe(1)
+    done()
+  })
+})
+
+test('wfsAt bad layer', done => {
+  expect.assertions(0)
+
+  const options = {
+    coordinate: COORD_AT_2_MTC,
+    wfsEndpoint: WFS_ENDPOINT,
+    layer: 'wrong',
+    geometryColumn: GEOM_COL,
+    dataProjection: DATA_PROJ
+  }
+  
+  wfsAt(options).then(features => {
+    expect(false).toBe(true)
+  }).catch(error => {
+    done()
+  })
+})
+
+test('wfsAt bad endpoint', done => {
+  expect.assertions(0)
+
+  const options = {
+    coordinate: COORD_AT_2_MTC,
+    wfsEndpoint: 'https://no-host/wfs',
+    layer: LAYER,
+    geometryColumn: GEOM_COL,
+    dataProjection: DATA_PROJ
+  }
+  
+  wfsAt(options).then(features => {
+    expect(false).toBe(true)
+  }).catch(error => {
     done()
   })
 })
--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const DEFAULT_QRY = 'service=WFS&version=2.0.0&request=GetFeature&typeNames=${la
 const DEFAULT_PROJ = 'EPSG:3857'
 
 const proj4 = require('proj4')
+const fetch = require('node-fetch')
 
 proj4.defs([
   ['EPSG:2263', '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=ft +to_meter=0.3048006096012192 +no_defs'],
   ['EPSG:6539', '+proj=lcc +lat_1=40.66666666666666 +lat_2=41.03333333333333 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000 +y_0=0 +ellps=GRS80 +units=us-ft +no_defs']
 ])
-
-require('isomorphic-fetch')
 
 const defaults = (options) => {
   if (!options.wfsEndpoint) {
@@ -47,6 +46,8 @@ module.exports = (options) => {
       }).catch(error => {
         reject(error)
       })
+    }).catch(error => {
+      reject(error)
     })
   })
 }
